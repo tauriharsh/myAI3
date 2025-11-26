b01c4a6dfa7ff0772c08a57c4e458f4237cf8148
tauriharsh <52110424+tauriharsh@users.noreply.github.com>
2025-11-26 23:17:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '50mb',
-    },
-  },
+  //
 };
 
 export default nextConfig;
